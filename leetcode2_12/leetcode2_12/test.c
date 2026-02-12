6f3786c831fd50a5dede70565e583251632ba7a1
Dawn-mowen <25mwduan@stu.edu.cn>
2026-02-12 22:54:46 +0800

    力扣2月12刷题

--- leetcode2_12/leetcode2_12/test.c
+++ leetcode2_12/leetcode2_12/test.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
@@ -69,76 +68,4 @@ bool isSubtree(struct TreeNode* root, struct TreeNode* subRoot)
     return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
 
 
-=======
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-//二叉树相等
-//bool isSameTree(struct TreeNode* p, struct TreeNode* q)
-//{
-//    if (p == NULL && q == NULL)
-//        return true;
-//
-//    if (p == NULL || q == NULL)
-//        return false;
-//
-//    if (p->val != q->val)
-//        return false;
-//
-//    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-//
-//}
-
-
-
-//对称二叉树
-//typedef struct TreeNode TreeNode;
-//bool isSameTree(struct TreeNode* p, struct TreeNode* q)
-//{
-//    if (p == NULL && q == NULL)
-//        return true;
-//
-//    if (p == NULL || q == NULL)
-//        return false;
-//
-//    if (p->val != q->val)
-//        return false;
-//
-//    return isSameTree(p->left, q->right) && isSameTree(p->right, q->left);
-//
-//}
-//
-//bool isSymmetric(struct TreeNode* root)
-//{
-//    return isSameTree(root->left, root->right);
-//
-//}
-
-
-bool isSameTree(struct TreeNode* p, struct TreeNode* q)
-{
-    if (p == NULL && q == NULL)
-        return true;
-
-    if (p == NULL || q == NULL)
-        return false;
-
-    if (p->val != q->val)
-        return false;
-
-    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-
-}
-
-bool isSubtree(struct TreeNode* root, struct TreeNode* subRoot)
-{
-    if (root == NULL)
-        return false;
-    if (root->val == subRoot->val && isSameTree(root, subRoot))
-        return true;
-
-    return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
-
-
->>>>>>> 2ca2a84ae91ce8e7e93ba456e2075bea07fceff6
 }
