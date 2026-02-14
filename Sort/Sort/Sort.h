22b3fc160bd0895e78757a3c9c47f609984c5eae
Dawn-mowen <25mwduan@stu.edu.cn>
2026-02-14 21:07:14 +0800

    排序算法

---
+++ Sort/Sort/Sort.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+
+void heapsort(int* a, int n);
+
+void AdjustDown(int* a, int n, int parent);
+
+void AdjustUp(int* a, int child);
+
+void Swap(int* p1, int* p2);
